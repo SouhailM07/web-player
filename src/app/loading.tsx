10fff2cf67d8473834b773bb539af0b754ef33c2
shadow <lightshadow416@gmail.com>
2024-07-02 16:21:23 +0100

    project core was built

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="loader-background">
+      <span className="loader">
+        <span className="loader-inner"></span>
+      </span>
+    </div>
+  );
+}
